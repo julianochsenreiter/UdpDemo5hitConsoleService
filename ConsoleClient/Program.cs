d354bf5bd8e78b7156590ff98274b5ed076e4be6
julianochsenreiter <74651560+julianochsenreiter@users.noreply.github.com>
2023-03-21 12:48:47 +0100

    finished

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,5 +1,30 @@
-﻿Console.WriteLine("Press a key to start ...");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UdpLibrary;
+
+Console.WriteLine("Press a key to start ...");
 Console.ReadKey();
+
+string message = "Hello students!";
+byte[] bytes = Encoding.UTF8.GetBytes(message);
+
+UdpClient client = new();
+IPEndPoint remoteEndpoint = new IPEndPoint(IPAddress.Loopback, 34567);
+await client.SendAsync(bytes, bytes.Length, remoteEndpoint);
+
+UdpReceiveResult response = await client.ReceiveAsync();
+byte[] bytes2 = response.Buffer;
+using(MemoryStream stream  = new(bytes2))
+{
+    using (BinaryReader reader = new(stream, Encoding.UTF8))
+    {
+        string firstname = reader.ReadString();
+        string lastname = reader.ReadString();
+        Person p = new(firstname, lastname);
+        Console.WriteLine(p);
+    }
+}
 
 // TODO: send a text message using the Encoding class
 // TODO: send a person object using a binary reader/writer
--- WpfService/MainWindow.xaml.cs
+++ WpfService/MainWindow.xaml.cs
@@ -1,16 +1,76 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using UdpLibrary;
 
 namespace WpfService
 {
     public partial class MainWindow : Window
     {
+        private readonly ObservableCollection<string> messages = new();
+        private Dictionary<IPEndPoint, long> counters = new();
+        private readonly object lockObject = new();
         public MainWindow()
         {
             InitializeComponent();
+            lbMessages.ItemsSource = messages;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            messages.Add("Service started ...");
+
+            _ = Task.Run(async () =>
+            {
+                IPEndPoint ep = new IPEndPoint(IPAddress.Loopback, 34567);
+                UdpClient client = new(ep);
+
+                while (true)
+                {
+                    UdpReceiveResult result = await client.ReceiveAsync();
+                    _ = Task.Factory.StartNew(async (object r) =>
+                    {
+                        var innerResult = (UdpReceiveResult)r;
+
+                        lock (lockObject)
+                        {
+                            if (counters.TryGetValue(innerResult.RemoteEndPoint, out long current))
+                                current++;
+                            else
+                                current = 1;
+                            counters[innerResult.RemoteEndPoint] = current;
+
+                        }
+
+
+                        byte[] bytes = innerResult.Buffer;
+                        string message = Encoding.UTF8.GetString(bytes);
+
+                        Dispatcher.Invoke(() => messages.Add(message));
+
+                        Person p = new Person("Clemens", "Kerer");
+
+                        using (MemoryStream stream = new())
+                        {
+                            using (BinaryWriter writer = new(stream, Encoding.UTF8))
+                            {
+                                writer.Write(p.Firstname);
+                                writer.Write(p.Lastname);
+                                writer.Close();
+                            }
+
+                            byte[] bytes2 = stream.ToArray();
+                            await client.SendAsync(bytes2, bytes2.Length, innerResult.RemoteEndPoint);
+                        }
+                    }, result);
+                }
+            });
+            
             // TODO: start the UDP service here
             // TODO: process each request on a separate thread
             // TODO: display the received message in the list box
